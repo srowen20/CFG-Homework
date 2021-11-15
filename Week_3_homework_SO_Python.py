f666e837bed43aeb3be1c26afca8541d08fce3cc
SophieOwen <sophierowen@outlook.com>
2021-11-15 16:55:09 +0000

    Week 4 homework main code and test files.

--- Week_3_homework_SO_Python.py
+++ Week_3_homework_SO_Python.py
@@ -56,7 +56,7 @@ my_money = input('How much money do you have? ')
 boat_cost = 20 + 5
 if float(my_money) > boat_cost:
     print('You can afford the boat hire')
-else :
+else:
     print('You cannot afford the boat hire')
 
 
